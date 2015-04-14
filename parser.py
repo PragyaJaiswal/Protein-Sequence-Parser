6fc1d4ce450426c8f377b5dc4c7ee70bcdc0c15a
PragyaJaiswal <pragya.jswl@gmail.com>
2015-04-14 22:35:29 +0530

    Using requests.

--- parser.py
+++ parser.py
@@ -6,7 +6,6 @@ import requests
 count={}
 
 i=0
-#a=0; c=0; d=0; e=0; f=0; g=0; h=0; i=0; k=0; l=0; m=0; n=0; p=0; q=0; r=0; s=0; t=0; v=0; w=0; x=0; y=0;
 
 outfile = open('/home/pragya/Documents/GitHub/Protein-Sequence-Parser/outfile.txt', 'w+')
 

bcc67914f2b479d90688ef9d695897a2a8f6aa0d
PragyaJaiswal <pragya.jswl@gmail.com>
2015-04-16 04:42:18 +0530

    Updated parser for accessing downloaded data and showing bar graphs for each of the mammals for which data is downloaded.

--- parser.py
+++ parser.py
@@ -1,65 +1,74 @@
 #!/usr/bin/python
 import re, os, json, requests, urllib, gzip
+from os import listdir
 import matplotlib.pyplot as plt
 from ftplib import FTP
+# from data import store
+# print(data.store)
 
-count={}
 
+# Location where the downloaded data is stored. Will be updated for import from that file.
+store = '/home/pragya/Documents/GitHub/Protein-Sequence-Parser/Data/Mammals/'
+
+'''
+Specify the location where you wish to store the files containing only the
+entire proteome sequence and not the FASTA format sequence.
+'''
+out = '/home/pragya/Documents/GitHub/Protein-Sequence-Parser/Output/'
+
+count={}	# Contains the frequency of occurence of each amino acid in the proteome sequence.
 i=0
 j=0
-#p=0
 
-outfile = open('/home/pragya/Documents/GitHub/Protein-Sequence-Parser/outfile.txt', 'w+')
+# urllib.urlretrieve('ftp://ftp.ensembl.org/pub/release-79/fasta/' + str(mammals[0]) + '/pep/Homo_sapiens.GRCh38.pep.abinitio.fa.gz', fullfilename)
 
-mammals = ['Homo sapiens', 'Pan troglodytes', 'Gorilla gorilla gorilla', 'Macaca mulatta', 'Rattus norvegicus', 'Mus musculus']
+# List of all the files that have been downloaded.
+files=os.listdir(store)
 
-for x in mammals:
-	case=x.lower()
-	mammals[j]=case.replace(' ', '_')
-	j+=1
-
-print(mammals)
-print(len(mammals))
-fullfilename = os.path.join('/home/pragya/Documents/GitHub/Protein-Sequence-Parser/Data/Mammals/','Homo_sapiens.GRCh38.pep.abinitio.fa.gz')
-urllib.urlretrieve('ftp://ftp.ensembl.org/pub/release-79/fasta/' + str(mammals[0]) + '/pep/Homo_sapiens.GRCh38.pep.abinitio.fa.gz', fullfilename)
-
-
-with gzip.open(fullfilename, 'r') as infile:
-	data=infile.readlines()
-	for line in data:
-		if line.startswith('>') or line.startswith('transcript_biotype'):
-			pass
-		else:
-			outfile.write(line)
-		print("Number of lines written: " + str(i))
-		i+=1
-	infile.close()
-	print("Amino acid sequence written to the requested file.")
-
-
-with open('/home/pragya/Documents/GitHub/Protein-Sequence-Parser/outfile.txt', 'r') as outfile:
-	print("Reading sequence from file.")
-	seq = outfile.read()
-	#print(seq)
-
-	"""
-	Counts the occurence of each
-	character in the sequence.
-	"""
-
-	for char in seq:
-		if char=='\n' or char=='*' or char=='X':
-			pass
-		else:
-			if char in count:
-				count[char] = count[char] + 1
+for file in files:
+	with gzip.open(store + str(file), 'r') as infile:
+		data=infile.readlines()
+		outfile = open(out + str((str(file)).split('.')[0]) + '.txt', 'w+')
+		for line in data:
+			if line.startswith('>') or line.startswith('transcript_biotype'):
+				pass
 			else:
-				count[char] = 1
-	print(count)
+				outfile.write(line)
+			print("Number of lines written: " + str(i))
+			i+=1
+		infile.close()
+		print("Amino acid sequence written to the requested file for file " + str(file))
 
-a=json.dumps(count, sort_keys=True, indent=4, separators=(',', ': '))
-print(a)
 
-plt.bar(range(len(count)), count.values(), align='center')
-plt.xticks(range(len(count)), list(count.keys()))
-plt.show()
+# List of all the files whose output sequence has been generated for the entire proteome, not in FASTA format.
+data=os.listdir(out)
+for file in data:
+	print(str(file))
+	with open(out + str(file), 'r') as outfile:
+		print("Reading sequence from file.")
+		seq = outfile.read()
+		# print(seq)
+
+		"""
+		Counts the occurence of each
+		character in the sequence.
+		"""
+
+		for char in seq:
+			if char=='\n' or char=='*' or char=='X':
+				pass
+			else:
+				if char in count:
+					count[char] = count[char] + 1
+				else:
+					count[char] = 1
+		print(count)
+
+	# Export dictionary to JSON for showing count in a presentable form.
+	a=json.dumps(count, sort_keys=True, indent=4, separators=(',', ': '))
+	print(a)
+
+	# Plot a bar graph for the number of each amino acid in the proteome sequence.
+	plt.bar(range(len(count)), count.values(), align='center')
+	plt.xticks(range(len(count)), list(count.keys()))
+	plt.show()

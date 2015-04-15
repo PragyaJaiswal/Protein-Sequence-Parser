7fb8c4c67f684128c7a26f96898ab16a6bebae2e
PragyaJaiswal <pragya.jswl@gmail.com>
2015-04-15 23:39:59 +0530

    Download sequence files using urllib from ensembl.

--- parser.py
+++ parser.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python
-import re, os, json
+import re, os, json, requests, urllib, gzip
 import matplotlib.pyplot as plt
-import requests
+from ftplib import FTP
 
 count={}
 
 i=0
+j=0
+#p=0
 
 outfile = open('/home/pragya/Documents/GitHub/Protein-Sequence-Parser/outfile.txt', 'w+')
 
-with open('/home/pragya/Downloads/Rattus_norvegicus.Rnor_5.0.pep.abinitio.fa', 'r') as infile:
-	data = infile.readlines()
+mammals = ['Homo sapiens', 'Pan troglodytes', 'Gorilla gorilla gorilla', 'Macaca mulatta', 'Rattus norvegicus', 'Mus musculus']
+
+for x in mammals:
+	case=x.lower()
+	mammals[j]=case.replace(' ', '_')
+	j+=1
+
+print(mammals)
+print(len(mammals))
+fullfilename = os.path.join('/home/pragya/Documents/GitHub/Protein-Sequence-Parser/Data/Mammals/','Homo_sapiens.GRCh38.pep.abinitio.fa.gz')
+urllib.urlretrieve('ftp://ftp.ensembl.org/pub/release-79/fasta/' + str(mammals[0]) + '/pep/Homo_sapiens.GRCh38.pep.abinitio.fa.gz', fullfilename)
+
+
+with gzip.open(fullfilename, 'r') as infile:
+	data=infile.readlines()
 	for line in data:
 		if line.startswith('>') or line.startswith('transcript_biotype'):
 			pass

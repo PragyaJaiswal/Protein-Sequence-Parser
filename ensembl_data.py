fd92641b13136b6ff7cc1e938fddd392ea35b398
PragyaJaiswal <pragya.jswl@gmail.com>
2015-04-17 21:59:51 +0530

    Functions made, main function added. Script can be imported.

--- ensembl_data.py
+++ ensembl_data.py
@@ -5,6 +5,9 @@ from ftplib import FTP
 '''
 Implemented for downloading 'proteome' sequence
 data, of 'mammals', from the Ensembl FTP server.
+
+Can be used for proteome data of other species also.
+Please modify the variable names as per the species.
 '''
 
 # Specify the mammals you wish to download data for in this.
@@ -22,73 +25,77 @@ mammals = [
 species = 'Mammals'
 store = '/home/pragya/Documents/GitHub/Protein-Sequence-Parser/Data/' + str(species) + '/'
 
-# Create the specified folder if it does not already exist.
-if not os.path.exists(store) and not store == '':
-	os.makedirs(store)
+def path_to_dir(store):
+	# Create the specified folder if it does not already exist.
+	if not os.path.exists(store) and not store == '':
+		os.makedirs(store)
 
-# If no directory is specified to store the data, store it on user's desktop.
-if store == '':
-	home = os.path.expanduser('~')
-	store = home + '/Desktop/' + str(species) + '/'
-	os.makedirs(store)
+	# If no directory is specified to store the data, store it on user's desktop.
+	if store == '':
+		home = os.path.expanduser('~')
+		store = home + '/Desktop/' + str(species) + '/'
+		os.makedirs(store)
 
-i = 0
-j = 0
-
-for x in mammals:
-	case = x.lower()
-	mammals[j] = case.replace(' ', '_')
-	j+=1
-
-print(mammals)
+def edit():
+	j = 0
+	for x in mammals:
+		case = x.lower()
+		mammals[j] = case.replace(' ', '_')
+		j+=1
+	print(mammals)
 
 
 '''
 FTP Stuff here.
 '''
-ftp_host = 'ftp.ensembl.org'
-ftp_user = 'anonymous'
-ftp_pass = ''
-ftp_path = '/pub/release-79/fasta'
+def ftp_download():
+	ftp_host = 'ftp.ensembl.org'
+	ftp_user = 'anonymous'
+	ftp_pass = ''
+	ftp_path = '/pub/release-79/fasta'
 
-ftp = FTP(ftp_host)
-ftp.login(ftp_user, ftp_pass)
-ftp.getwelcome()
-ftp.cwd(ftp_path)
+	ftp = FTP(ftp_host)
+	ftp.login(ftp_user, ftp_pass)
+	ftp.getwelcome()
+	ftp.cwd(ftp_path)
 
-dirs = ftp.nlst()
-# print(dirs)
+	dirs = ftp.nlst()
+	# print(dirs)
 
-# Navigate to the required diretory and thereby download data.
-for dir in dirs:
-	for p in range(0,len(mammals)):
-		if re.search(str(mammals[p]), dir):
-			print(str(mammals[p]) + ' present.')
-			path=ftp_path + '/' + str(mammals[p])
-			ftp.cwd(path)
-			types=ftp.nlst()
-			print(types)
-			# if str(mammals[p]) in seq_type:
-			# 	pass
-			# else:
-			# 	seq_type[str(mammals[p])]=ftp.nlst()
-			for i in types:
-				if re.search('pep', i):
-					prefinal = path + '/' + str(i)
-					ftp.cwd(prefinal)
-					print(ftp.pwd())
-					files = ftp.nlst()
-					print(files)
-					
-					for i in files:
-						print('here1')
-						if re.search('abinitio', i):
-							print('here2')
-							final = prefinal + '/' + str(i)
-							fullfilename = os.path.join(store + str(i))
-							urllib.urlretrieve('ftp://' + ftp_host + str(final), fullfilename)
-							# ftp.retrbinary('RETR ' + str(i), callback=None)
-							break
-		p+=1
+	# Navigate to the required directory on the server and thereby download data.
+	for dir in dirs:
+		for p in range(0,len(mammals)):
+			if re.search(str(mammals[p]), dir):
+				print(str(mammals[p]) + ' present.')
+				path=ftp_path + '/' + str(mammals[p])
+				ftp.cwd(path)
+				types=ftp.nlst()
+				print(types)
+				# if str(mammals[p]) in seq_type:
+				# 	pass
+				# else:
+				# 	seq_type[str(mammals[p])]=ftp.nlst()
+				for i in types:
+					if re.search('pep', i):
+						prefinal = path + '/' + str(i)
+						ftp.cwd(prefinal)
+						print(ftp.pwd())
+						files = ftp.nlst()
+						print(files)
+						
+						for i in files:
+							if re.search('abinitio', i):
+								final = prefinal + '/' + str(i)
+								fullfilename = os.path.join(store + str(i))
+								urllib.urlretrieve('ftp://' + ftp_host + str(final), fullfilename)
+								# ftp.retrbinary('RETR ' + str(i), callback=None)
+								break
+			p+=1
 
-print(ftp.pwd())
+	print(ftp.pwd())
+
+if __name__ == '__main__':
+	main()
+	path_to_dir(store)
+	edit()
+	ftp_download()

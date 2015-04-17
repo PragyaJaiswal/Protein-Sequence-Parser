01b83ab40c261d4fa1a8c6f489ff651f2662629a
PragyaJaiswal <pragya.jswl@gmail.com>
2015-04-17 14:38:52 +0530

    Make directory if not exists. Download to desktop, if folder not specified.

--- ensembl_data.py
+++ ensembl_data.py
@@ -9,9 +9,6 @@ data, of 'mammals', from the Ensembl FTP server.
 
 # Specify the mammals you wish to download data for in this.
 # Only provide scientific names for each of them, as shown.
-
-species = 'Mammals'
-
 mammals = [
 	'Homo sapiens',
 	'Pan troglodytes',
@@ -22,7 +19,18 @@ mammals = [
 ]
 
 # Specify the location where you wish to store the downloaded data.
+species = 'Mammals'
 store = '/home/pragya/Documents/GitHub/Protein-Sequence-Parser/Data/' + str(species) + '/'
+
+# Create the specified folder if it does not already exist.
+if not os.path.exists(store) and not store == '':
+	os.makedirs(store)
+
+# If no directory is specified to store the data, store it on user's desktop.
+if store == '':
+	home = os.path.expanduser('~')
+	store = home + '/Desktop/' + str(species) + '/'
+	os.makedirs(store)
 
 i = 0
 j = 0
@@ -35,52 +43,52 @@ for x in mammals:
 print(mammals)
 
 
-'''
-FTP Stuff here.
-'''
-ftp_host = 'ftp.ensembl.org'
-ftp_user = 'anonymous'
-ftp_pass = ''
-ftp_path = '/pub/release-79/fasta'
+# '''
+# FTP Stuff here.
+# '''
+# ftp_host = 'ftp.ensembl.org'
+# ftp_user = 'anonymous'
+# ftp_pass = ''
+# ftp_path = '/pub/release-79/fasta'
 
-ftp = FTP(ftp_host)
-ftp.login(ftp_user, ftp_pass)
-ftp.getwelcome()
-ftp.cwd(ftp_path)
+# ftp = FTP(ftp_host)
+# ftp.login(ftp_user, ftp_pass)
+# ftp.getwelcome()
+# ftp.cwd(ftp_path)
 
-dirs = ftp.nlst()
-# print(dirs)
+# dirs = ftp.nlst()
+# # print(dirs)
 
-# Navigate to the required diretory and thereby download data.
-for dir in dirs:
-	for p in range(0,len(mammals)):
-		if re.search(str(mammals[p]), dir):
-			print(str(mammals[p]) + ' present.')
-			path=ftp_path + '/' + str(mammals[p])
-			ftp.cwd(path)
-			types=ftp.nlst()
-			print(types)
-			# if str(mammals[p]) in seq_type:
-			# 	pass
-			# else:
-			# 	seq_type[str(mammals[p])]=ftp.nlst()
-			for i in types:
-				if re.search('pep', i):
-					prefinal = path + '/' + str(i)
-					ftp.cwd(prefinal)
-					print(ftp.pwd())
-					files = ftp.nlst()
-					print(files)
+# # Navigate to the required diretory and thereby download data.
+# for dir in dirs:
+# 	for p in range(0,len(mammals)):
+# 		if re.search(str(mammals[p]), dir):
+# 			print(str(mammals[p]) + ' present.')
+# 			path=ftp_path + '/' + str(mammals[p])
+# 			ftp.cwd(path)
+# 			types=ftp.nlst()
+# 			print(types)
+# 			# if str(mammals[p]) in seq_type:
+# 			# 	pass
+# 			# else:
+# 			# 	seq_type[str(mammals[p])]=ftp.nlst()
+# 			for i in types:
+# 				if re.search('pep', i):
+# 					prefinal = path + '/' + str(i)
+# 					ftp.cwd(prefinal)
+# 					print(ftp.pwd())
+# 					files = ftp.nlst()
+# 					print(files)
 					
-					for i in files:
-						print('here1')
-						if re.search('abinitio', i):
-							print('here2')
-							final = prefinal + '/' + str(i)
-							fullfilename = os.path.join(store + str(i))
-							urllib.urlretrieve('ftp://' + ftp_host + str(final), fullfilename)
-							# ftp.retrbinary('RETR ' + str(i), callback=None)
-							break
-		p+=1
+# 					for i in files:
+# 						print('here1')
+# 						if re.search('abinitio', i):
+# 							print('here2')
+# 							final = prefinal + '/' + str(i)
+# 							fullfilename = os.path.join(store + str(i))
+# 							urllib.urlretrieve('ftp://' + ftp_host + str(final), fullfilename)
+# 							# ftp.retrbinary('RETR ' + str(i), callback=None)
+# 							break
+# 		p+=1
 
-print(ftp.pwd())
+# print(ftp.pwd())

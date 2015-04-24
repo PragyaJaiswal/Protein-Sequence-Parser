32663437fa5f3040247357e1b23c80807abb6964
Prashant Sinha <prashnts@live.com>
2015-04-25 01:43:40 +0530

    Fixes

--- graph.py
+++ graph.py
@@ -34,18 +34,25 @@ mammals = [
 ]
 
 out = {}
+out2 = ""
 
 for specie in mammals:
     file_name = "stage/" + specie + ".txt"
 
     out[specie] = {}
+    out2 += specie + ","
 
     with open(file_name, "r") as minion:
         file_dat = minion.read()
         for acid in amino_acids:
             out[specie][acid] = file_dat.count(acid)
+            out2 += str(out[specie][acid]) + ","
+    out2 += "\n"
 
 out_data = json.dumps(out, indent=4)
 
 with open("stage/out.json", "w") as minion:
     minion.write(out_data)
+
+with open("stage/out.csv", "w") as minion:
+    minion.write(out2)

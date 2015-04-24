3c20e16324b5b6edf3bb0bb831d7079001e8fa0c
Prashant Sinha <prashnts@live.com>
2015-04-24 23:25:22 +0530

    Added the graph scaffold

---
+++ graph.py
@@ -0,0 +1,32 @@
+
+amino_acids = {
+    'I': None,
+    'L': None,
+    'V': None,
+    'F': None,
+    'M': None,
+    'C': None,
+    'A': None,
+    'G': None,
+    'P': None,
+    'T': None,
+    'S': None,
+    'Y': None,
+    'W': None,
+    'Q': None,
+    'N': None,
+    'H': None,
+    'E': None,
+    'D': None,
+    'K': None,
+    'R': None,
+    'X': None,
+}
+
+mammal = {
+    'Homo_Sapiens': None,
+    'Macaca_mulatta': None,
+    'Mus_musculus': None,
+    'Pan_troglodytes': None,
+    'Rattus_norvegicus': None,
+}

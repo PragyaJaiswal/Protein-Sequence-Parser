0eeca1f7f484faca597226ad89c27fd3c726e243
Prashant Sinha <prashnts@live.com>
2015-04-25 00:16:21 +0530

    Finishes the graph.py

--- graph.py
+++ graph.py
@@ -1,54 +1,51 @@
 #!/usr/bin/env python3
 import json
 
-amino_acids = {
-    'I': None,
-    'L': None,
-    'V': None,
-    'F': None,
-    'M': None,
-    'C': None,
-    'A': None,
-    'G': None,
-    'P': None,
-    'T': None,
-    'S': None,
-    'Y': None,
-    'W': None,
-    'Q': None,
-    'N': None,
-    'H': None,
-    'E': None,
-    'D': None,
-    'K': None,
-    'R': None,
-    'X': None,
-}
+amino_acids = [
+    'I',
+    'L',
+    'V',
+    'F',
+    'M',
+    'C',
+    'A',
+    'G',
+    'P',
+    'T',
+    'S',
+    'Y',
+    'W',
+    'Q',
+    'N',
+    'H',
+    'E',
+    'D',
+    'K',
+    'R',
+    'X',
+]
 
-mammal = {
-    'Homo_sapiens': None,
-    'Macaca_mulatta': None,
-    'Mus_musculus': None,
-    'Pan_troglodytes': None,
-    'Rattus_norvegicus': None,
-}
+mammals = [
+    'Homo_sapiens',
+    'Macaca_mulatta',
+    'Mus_musculus',
+    'Pan_troglodytes',
+    'Rattus_norvegicus',
+]
 
-out_mammal = mammal
+out = {}
 
-for specie, acids in mammal.items():
+for specie in mammals:
     file_name = "stage/" + specie + ".txt"
+
+    out[specie] = {}
+
     with open(file_name, "r") as minion:
         file_dat = minion.read()
-        print(file_name)
-        amino_acids_specie = amino_acids
-        for acid, count in amino_acids_specie.items():
-            amino_acids_specie[acid] = file_dat.count(acid)
-            break
-        print(out_mammal)
+        for acid in amino_acids:
+            out[specie][acid] = file_dat.count(acid)
 
-        out_mammal[specie] = amino_acids_specie
-
-out_data = json.dumps(out_mammal, indent=4)
+out_data = json.dumps(out, indent=4)
 
 with open("stage/out.json", "w") as minion:
     minion.write(out_data)

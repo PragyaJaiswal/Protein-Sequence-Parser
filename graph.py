817a78dc470a68b5acd4af6bec0f6350feddfc7b
Prashant Sinha <prashnts@live.com>
2015-04-25 00:08:54 +0530

    Fixes

--- graph.py
+++ graph.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+import json
 
 amino_acids = {
     'I': None,
@@ -24,9 +26,29 @@ amino_acids = {
 }
 
 mammal = {
-    'Homo_Sapiens': None,
+    'Homo_sapiens': None,
     'Macaca_mulatta': None,
     'Mus_musculus': None,
     'Pan_troglodytes': None,
     'Rattus_norvegicus': None,
 }
+
+out_mammal = mammal
+
+for specie, acids in mammal.items():
+    file_name = "stage/" + specie + ".txt"
+    with open(file_name, "r") as minion:
+        file_dat = minion.read()
+        print(file_name)
+        amino_acids_specie = amino_acids
+        for acid, count in amino_acids_specie.items():
+            amino_acids_specie[acid] = file_dat.count(acid)
+            break
+        print(out_mammal)
+
+        out_mammal[specie] = amino_acids_specie
+
+out_data = json.dumps(out_mammal, indent=4)
+
+with open("stage/out.json", "w") as minion:
+    minion.write(out_data)

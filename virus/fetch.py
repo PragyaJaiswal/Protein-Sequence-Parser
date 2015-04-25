5207cc1a27080efdecaa37bdabdfba32d19b659c
PragyaJaiswal <pragya.jswl@gmail.com>
2015-04-25 13:32:39 +0530

    Parse and write the proteome seq which is not in FASTA format in a separate file.

--- virus/fetch.py
+++ virus/fetch.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
-import re, os, json, urllib, requests, httplib2
+import re, os, json, sys, gzip, urllib, requests, httplib2
 from bs4 import BeautifulSoup
 from ftplib import FTP
+
+sys.path.insert(0, '/home/pragya/Documents/GitHub/Protein-Sequence-Parser/')
+import parser
+
+# parser.viruses()
 
 '''
 Script to find proteomes of all viruses whose hosts are
@@ -10,6 +15,7 @@ http://viralzone.expasy.org/all_by_species/678.html
 Go to the respective virus proteome on uniprot and download the
 proteome of the virus.
 '''
+
 global redirect, viruses, output
 redirect = []
 viruses = []
@@ -42,8 +48,8 @@ def uniprot(urls):
 					viruses.append(str(tax_num))
 			output = remove_duplicates(viruses)
 			print(output)
-		# if num == 3:
-		# 	break
+		if num == 3:
+			break
 	print('No. of preoteomes to be retrieved: ' + str(num))
 	return output
 
@@ -56,10 +62,6 @@ def remove_duplicates(viruses):
 			output.append(virus)
 			visited.add(virus)
 	return output
-
-
-def taxonomy(output):
-	pass
 
 
 '''
@@ -112,10 +114,65 @@ def path_to_dir(out):
 		os.makedirs(out)
 
 
+def taxonomy(store):
+	i = 0
+	j = 0
+
+	'''
+	Specify the location where you wish to store the files containing only the
+	entire proteome sequence and not the FASTA format sequence.
+	'''	
+	out = './out/' + 'Viruses/'
+
+	path_to_dir(out)
+
+	# List of all the files that have been downloaded.	
+	files=os.listdir(store)
+
+	print("Virus proteomes being processed.")
+
+	for file in files:
+		j+=1
+		print(j)
+
+		# print(bool(re.search('additional', str(file))))
+
+		if bool(re.search('additional', file)):
+			with gzip.open(store + str(file), 'r') as reading:
+				data = reading.readlines()
+				print('Pre: ' + str(pre))
+				prev = open(str(pre), 'w+')
+				for line in data:
+					if line.startswith('>') or line.startswith('transcript_biotype'):
+						pass
+					else:
+						prev.write(line)
+		else:
+			with gzip.open(store + str(file), 'r') as infile:
+				data = infile.readlines()
+				filename = out + str((str(file)).split('.')[0]) + '.txt'
+				outfile = open(out + str((str(file)).split('.')[0]) + '.txt', 'w+')
+				# print(str(outfile))
+				for line in data:
+					if line.startswith('>') or line.startswith('transcript_biotype'):
+						pass
+					else:
+						outfile.write(line)
+					# print("Number of lines written: " + str(i))
+					i+=1
+				pre = filename
+				infile.close()
+				print("Amino acid sequence written to the requested file for file " + str(file))
+		if j == 10:
+			break
+	# parse(out, str(species))
+
+
 if __name__ == '__main__':
 	url = 'http://viralzone.expasy.org/all_by_species/678.html'
-	store = './dat/'
+	store = './dat/human_virus/'
 	path_to_dir(store)
 	urls = init(url)
 	output = uniprot(urls)
 	ftp_download(output)
+	taxonomy(store)

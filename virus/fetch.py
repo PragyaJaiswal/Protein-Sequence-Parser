9858615754facff4e30e67a2e50aae689e3938f3
PragyaJaiswal <pragya.jswl@gmail.com>
2015-04-25 09:54:33 +0530

    Visit the uniprot URL for the proteome, retrieve the virus' taxonomical number.

--- virus/fetch.py
+++ virus/fetch.py
@@ -9,18 +9,62 @@ http://viralzone.expasy.org/all_by_species/678.html
 Go to the respective virus proteome on uniprot and download the
 proteome of the virus.
 '''
+global redirect, viruses, output
+redirect = []
+viruses = []
+output = []
 
-url = 'http://viralzone.expasy.org/all_by_species/678.html'
-response = requests.get(url)
+def init(url):
+	print('Getting: ' + str(url))
+	response = requests.get(url)
 
-soup = BeautifulSoup(response.content)
+	soup = BeautifulSoup(response.content)
 
-print(soup.prettify())
-num = 0
+	# print(soup.prettify())
+	num = 0
 
-for link in soup.find_all('a'):
-	redirect = link.get('href')
-	if re.search('uniprot', str(redirect)):
+	for link in soup.find_all('a'):
+		redirect.append(str(link.get('href')))
 		num+=1
-		print(redirect)
-print('No. of preoteomes to be retrieved: ' + str(num))
+	# print(redirect)
+	return redirect
+
+		
+def uniprot(urls):
+	num = 0
+	for x in urls:
+		if re.search('uniprot', str(x)):
+			num+=1
+			res = init(x)
+			for y in res:
+				if re.search('taxonomy/', str(y)):
+					tax_num = str(y.split('/')[2])
+					viruses.append(str(tax_num))
+			output = remove_duplicates(viruses)
+					# else:
+					# 	for x in viruses:
+					# 		if not tax_num == x:
+					# 			viruses.append(str(y.split('/')[2]))
+					# 		else:
+					# 			pass
+			print(output)
+	print('No. of preoteomes to be retrieved: ' + str(num))
+
+
+def remove_duplicates(viruses):
+	output = []
+	visited = set()
+	for virus in viruses:
+		if virus not in visited:
+			output.append(virus)
+			visited.add(virus)
+	return output
+
+
+def taxonomy():
+	pass
+
+if __name__ == '__main__':
+	url = 'http://viralzone.expasy.org/all_by_species/678.html'
+	urls = init(url)
+	uniprot(urls)
